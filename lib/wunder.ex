55a6934eb2e0703b1d8ca7bcd5291c70548f1c7d
denispeplin <denis.peplin@gmail.com>
2018-06-16 11:04:03 +0300

    mix new --sup wunder

---
+++ lib/wunder.ex
@@ -0,0 +1,18 @@
+defmodule Wunder do
+  @moduledoc """
+  Documentation for Wunder.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wunder.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
